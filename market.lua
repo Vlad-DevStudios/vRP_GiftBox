e564181b7db3e16c2505fd67ffb091f33dd25eee
Vlad-DevStudios <42617246+Vlad-DevStudios@users.noreply.github.com>
2019-02-16 17:16:45 +0200

    Update market.lua

--- market.lua
+++ market.lua
@@ -11,27 +11,30 @@ function giftbox_DisplayText(str)
 	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
 end
 
-function DrawTxt(x,y,z,textInput,fontId,scaleX,scaleY)
-	local px,py,pz=table.unpack(GetGameplayCamCoords())
-	local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)    
-	local scale = (1/dist)*20
-	local fov = (1/GetGameplayCamFov())*100
-	local scale = scale*fov   
-	SetTextScale(scaleX*scale, scaleY*scale)
-	SetTextFont(fontId)
-	SetTextProportional(1)
-	SetTextColour(0, 255, 0, 255)		
-	SetTextDropshadow(1, 1, 1, 1, 255)
-	SetTextEdge(2, 0, 0, 0, 150)
-	SetTextDropShadow()
-	SetTextOutline()
-	SetTextEntry("STRING")
-	SetTextCentre(1)
-	AddTextComponentString(textInput)
-	SetDrawOrigin(x,y,z+2, 0)
-	DrawText(0.0, 0.0)
-	ClearDrawOrigin()
-   end
+function DrawTxt(x, y, z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+
+    local scale = (1/dist)*2
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+
+    if onScreen then
+        SetTextScale(0.0*scale, 0.7*scale)
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+    end
+end
 
 Citizen.CreateThread(function() 
 	while true do
@@ -41,8 +44,9 @@ Citizen.CreateThread(function()
 		local px,py,pz = playerPos.x, playerPos.y, playerPos.z
 		
 		if GetDistanceBetweenCoords(-545.720703125,-227.97738647461,37.649803161621,px,py,pz,true) <= 150 then
-			DrawTxt(-545.720703125,-227.97738647461,37.649803161621 +0.10,"GiftBox Market\nBuy GiftBoxes with money\n 1.000.000 $", 10, 0.1, 0.1) 
-			DrawMarker(29,-545.720703125,-227.97738647461,37.649803161621+0.5,0,0,0,0,0,0,2.7,2.5,2.7,0,255,0,130,0,0,0,true)
+		    DrawTxt(-545.720703125,-227.97738647461,37.649803161621 +2.40, tostring("~w~[~g~GiftBox~w~]\n~g~Market\n~w~Buy ~g~GiftBoxes ~w~with ~g~money\n ~g~1.000.000 $"))
+			DrawMarker(29,-545.720703125,-227.97738647461,37.649803161621+0.5,0, 0, 0, 0, 0, 0, 1.5,1.5,1.5, 0,255,0,130,0,0,0,true)
+			DrawMarker(6,-545.720703125,-227.97738647461,37.649803161621+0.5,0, 0, 0, 0, 0, 0, 2.0,2.0,2.0, 255,255,255,130,0,0,0,true)
 		end
 		if(Vdist(-545.720703125,-227.97738647461,37.649803161621,px,py,pz) < 2)then
 			if (incircle == false) then
